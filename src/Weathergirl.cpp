5c7a6bf62851bd6036644fba83d4b8fc2ae4c31e
Eliza Weisman <hi@hawkweisman.me>
2017-05-08 11:18:52 -0400

    feat(DHT11): add quick DHT11 test code

--- src/Weathergirl.cpp
+++ src/Weathergirl.cpp
@@ -8,4 +8,61 @@
  */
 
 #include <Arduino.h>
+
+// ThreeClor.h, by Eliza Weisman
+// for easy HSV control of RGB LED modules
 #include <ThreeClor.h>
+
+// Adafruit_Sensor and DHT libraries, by Adafruit
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
+
+const int DHT_PIN = 13
+        , DHT_TYPE = DHT11;
+
+CommonCathodeLed<11,10, 9> rgb_led = CommonCathodeLed<11, 10, 9>();
+DHT dht(DHT_PIN, DHT_TYPE);
+
+void setup() {
+    Serial.begin(9600);
+    dht.begin();
+}
+
+void loop() {
+    // Wait a few seconds between measurements.
+    delay(2000);
+
+    // Reading temperature or humidity takes about 250 milliseconds!
+    // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+    float h = dht.readHumidity();
+    // Read temperature as Celsius (the default)
+    float t = dht.readTemperature();
+    // Read temperature as Fahrenheit (isFahrenheit = true)
+    float f = dht.readTemperature(true);
+
+    // Check if any reads failed and exit early (to try again).
+    if (isnan(h) || isnan(t) || isnan(f)) {
+      Serial.println("Failed to read from DHT sensor!");
+      return;
+    }
+
+    // Compute heat index in Fahrenheit (the default)
+    float hif = dht.computeHeatIndex(f, h);
+    // Compute heat index in Celsius (isFahreheit = false)
+    float hic = dht.computeHeatIndex(t, h, false);
+
+    Serial.print("Humidity: ");
+    Serial.print(h);
+    Serial.print(" %\t");
+    Serial.print("Temperature: ");
+    Serial.print(t);
+    Serial.print(" °C ");
+    Serial.print(f);
+    Serial.print(" °F\t");
+    Serial.print("Heat index: ");
+    Serial.print(hic);
+    Serial.print(" °C ");
+    Serial.print(hif);
+    Serial.println(" °F");
+}
