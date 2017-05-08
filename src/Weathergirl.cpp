c7ddab0bda88264ef6501d8535ebacc5a387b3fd
Eliza Weisman <hi@hawkweisman.me>
2017-05-08 11:10:06 -0400

    fix: disable ethernet lib for now (we're not using it yet)

---
+++ src/Weathergirl.cpp
@@ -0,0 +1,11 @@
+/*
+    WeatherGirl
+    by Eliza Weisman
+    http://elizas.website
+
+    A cute little DIY weather station (with fancy lighting!)
+
+ */
+
+#include <Arduino.h>
+#include <ThreeClor.h>
