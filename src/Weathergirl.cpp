81c9836f2a5283c3826fb1a2b51653c32c28be49
Eliza Weisman <hi@hawkweisman.me>
2017-05-10 11:00:14 -0400

    refac: break stuff into functions

--- src/Weathergirl.cpp
+++ src/Weathergirl.cpp
@@ -73,7 +73,7 @@ float h, t, f, hif, hic;
 CommonCathodeLed<11,10, 9> rgb_led = CommonCathodeLed<11, 10, 9>();
 DHT dht(DHT_PIN, DHT_TYPE);
 
-void setup() {
+void setup(void) {
     // start the SPI library:
     SPI.begin();
 
@@ -95,37 +95,35 @@ void setup() {
     dht.begin();
 }
 
-void loop() {
-    // Wait a few seconds between measurements.
-    delay(2000);
-
+void measure(void) {
+    // TODO: refactor this to a class method
     // Reading temperature or humidity takes about 250 milliseconds!
     // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
-    float h = dht.readHumidity();
-    // Read temperature as Celsius (the default)
-    float t = dht.readTemperature();
-    // Read temperature as Fahrenheit (isFahrenheit = true)
-    float f = dht.readTemperature(true);
+    float h_temp = dht.readHumidity()
+          // Read temperature as Celsius (the default)
+        , t_temp = dht.readTemperature()
+          // Read temperature as Fahrenheit (isFahrenheit = true)
+        , f_temp = dht.readTemperature(true)
+        ;
 
     // Check if any reads failed and exit early (to try again).
-    if (isnan(h) || isnan(t) || isnan(f)) {
+    if (isnan(h_temp) || isnan(t_temp) || isnan(f_temp)) {
       Serial.println("Failed to read from DHT sensor!");
       lcd.clear();
       lcd.print("DHT sensor error!");
       return;
-    }
+  } else {
+      h = h_temp;
+      t = t_temp;
+      f = f_temp;
+      hif = dht.computeHeatIndex(f, h);
+      hic = dht.computeHeatIndex(t, h, false);
+  }
 
-    // int hue = map((long)f, TEMP_MIN, TEMP_MAX, 0, 255);
-    // // set the LED color
-    // rgb_led.color = HSVColor(hue);
-    // rgb_led.show();
+}
 
-    // Compute heat index in Fahrenheit (the default)
-    float hif = dht.computeHeatIndex(f, h);
-    // Compute heat index in Celsius (isFahreheit = false)
-    float hic = dht.computeHeatIndex(t, h, false);
-
-
+void serial_output(void) {
+    // TODO: refactor this to a class method
     Serial.print("Humidity: ");
     Serial.print(h);
     Serial.print(" %\t");
@@ -153,4 +151,12 @@ void loop() {
     lcd.print("humidity: ");
     lcd.print(h);
     lcd.print("%");
+}
+
+void loop(void) {
+    // Wait a few seconds between measurements.
+    delay(2000);
+    measure();
+    serial_output();
+
 }
