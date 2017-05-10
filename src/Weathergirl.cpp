c239acde17393e808cc229088a4c78305218245c
Eliza Weisman <hi@hawkweisman.me>
2017-05-10 10:40:33 -0400

    fix: make things that can't be const not const

--- src/Weathergirl.cpp
+++ src/Weathergirl.cpp
@@ -35,7 +35,7 @@
 
 // Enter a MAC address and IP address for your controller below.
 // The IP address will be dependent on your local network:
-const byte mac[] = {
+byte mac[] = {
   0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
 };
 
